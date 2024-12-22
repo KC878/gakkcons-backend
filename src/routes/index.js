54a11f439cbf049ad19c144061c0d127e0643297
margiefabores <faboresm@gmail.com>
2024-12-22 14:21:52 +0800

    added check auth middleware to teacher routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const user = require("./user");
 const appointment = require("./appointment");
+const teacher = require("./teacher");
 
 const api = express.Router();
 
-api.use("/user", user);
-api.use("/appointment", appointment);
+api.use("/users", user);
+api.use("/appointments", appointment);
+api.use("/teachers", teacher);
 
 module.exports = api;
--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const { getTeachers, searchTeacher } = require("../controllers/teacher");
+const checkAuth = require("../middlewares/auth");
 
 const teacher = express.Router();
 
-teacher.get("/", getTeachers);
-teacher.get("/search", searchTeacher);
+teacher.get("/", checkAuth, getTeachers);
+teacher.get("/search", checkAuth, searchTeacher);
 
 module.exports = teacher;
