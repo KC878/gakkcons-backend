e7a76e60c2cbd802ed9f2c4029a19bd4692d044e
JandreiX <jiggerandreigobenza@gmail.com>
2025-01-05 21:18:12 +0800

    Added completed appointments

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -255,7 +255,7 @@ const getProfile = async (req, res) => {
 const updateProfile = async (req, res) => {
   try {
     const userId = req.user.user_id;
-    const { firstName, lastName, email, currentPassword, newPassword } =
+    const { firstName, lastName, email, currentPassword,   } =
       req.body;
 
     await pool.query("BEGIN");
