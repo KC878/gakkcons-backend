7ac8c89bcc7bff39d8fc6eb3d6914b119ed4b198
margiefabores <faboresm@gmail.com>
2025-01-05 12:41:07 +0800

    added search function in get teachers

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -1,11 +1,10 @@
 const express = require("express");
 
-const { getTeachers, searchTeacher } = require("../controllers/teacher");
+const { getTeachers } = require("../controllers/teacher");
 const checkAuth = require("../middlewares/auth");
 
 const teacher = express.Router();
 
 teacher.get("/", checkAuth, getTeachers);
-teacher.get("/search", checkAuth, searchTeacher);
 
 module.exports = teacher;
