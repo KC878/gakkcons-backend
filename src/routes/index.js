723e87b02591009c1eb199be4ed6687c6fb2fa10
JandreiX <102495959+JandreiX@users.noreply.github.com>
2025-01-14 10:29:01 +0000

    Deployment feature - get Notif/Appointment route checking
    Check if routes meet the endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ const user = require("./user");
 const appointment = require("./appointment");
 const teacher = require("./teacher");
 const notification = require("./notification");
+const notificationController = require("../controllers/notification");
+const appointmentController = require("../controllers/appointment");
 
 const api = express.Router();
 
@@ -11,5 +13,7 @@ api.use("/users", user);
 api.use("/appointments", appointment);
 api.use("/teachers", teacher);
 api.use("/notifications", notification);
+api.get("/notifications", notificationController.getNotifications);
+api.get("/appointments", appointmentController.getAppointments);
 
 module.exports = api;
