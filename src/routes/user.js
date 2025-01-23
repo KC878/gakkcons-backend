4a41035003c8b3f48b06c30d6ae2b505424676f2
JandreiX <jiggerandreigobenza@gmail.com>
2025-01-24 02:01:24 +0800

    Added update and edit users

--- src/routes/user.js
+++ src/routes/user.js
@@ -11,19 +11,31 @@ const {
   verifyUser,
   updatePreferMode,
   changePassword,
+  signupUserByAdmin,
+  getSubjects,
+  getUsers,
+  deleteUser,
+  updateUser,
+  
 } = require("../controllers/user");
 
 const user = express.Router();
 
 user.post("/login", loginUser);
 user.post("/signup", signupUser);
+user.post("/signupadmin", signupUserByAdmin);
 user.post("/verify", verifyUser);
 user.post("/password/forgot", forgotPassword);
 user.post("/password/reset", resetPassword);
 user.post("/password/forgot/reset", changePassword);
 user.get("/profile", checkAuth, getProfile);
 user.put("/profile/update", checkAuth, updateProfile);
-user.put("/profile/mode/update", checkAuth, updatePreferMode)
+user.put("/profile/mode/update", checkAuth, updatePreferMode);
+user.get("/subjects", getSubjects);
+user.get('/getUsers', getUsers);
+user.delete('/delete/:user_id', checkAuth, deleteUser);
+user.put("/edit/:user_id", checkAuth, updateUser);
+
 
 
 module.exports = user;
