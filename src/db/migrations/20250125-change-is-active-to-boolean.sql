204f30a32032b45f823dce7930a9b9d0b5df8e54
margiefabores <faboresm@gmail.com>
2025-01-29 12:36:46 +0800

    fix minor error in changing is_active column data type migration script

--- src/db/migrations/20250125-change-is-active-to-boolean.sql
+++ src/db/migrations/20250125-change-is-active-to-boolean.sql
@@ -1,4 +1,5 @@
 DO $$
+DECLARE is_active_data_type TEXT;
 BEGIN
     IF EXISTS (
         SELECT 1
@@ -6,11 +7,18 @@ BEGIN
         WHERE LOWER(table_name) = 'users' AND LOWER(column_name) = 'is_active'
     ) THEN
   
-        ALTER TABLE users ALTER COLUMN is_active DROP DEFAULT;
+        SELECT data_type INTO is_active_data_type 
+        FROM information_schema.columns 
+        WHERE LOWER(table_name) = 'users' 
+        AND LOWER(column_name) = 'is_active';
 
-        ALTER TABLE users ALTER COLUMN is_active TYPE BOOLEAN USING (is_active = 1);
+        IF is_active_data_type = 'integer' THEN
+            ALTER TABLE users ALTER COLUMN is_active DROP DEFAULT;
 
-        ALTER TABLE users ALTER COLUMN is_active SET DEFAULT FALSE;
+            ALTER TABLE users ALTER COLUMN is_active TYPE BOOLEAN USING (is_active = 1);
+
+            ALTER TABLE users ALTER COLUMN is_active SET DEFAULT FALSE;
+        END IF;
     ELSE
         ALTER TABLE users ADD COLUMN is_active BOOLEAN DEFAULT FALSE;
     END IF;
